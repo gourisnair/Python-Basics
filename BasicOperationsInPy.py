a921c215e6c07bbca98a52650cf96fec10699911
gourisnair <gouri.gopan@gmail.com>
2018-10-12 16:49:58 +0530

    Basic operatrions

---
+++ BasicOperationsInPy.py
@@ -0,0 +1,8 @@
+print "Hello, World"
+myName = raw_input("what is your name ")
+print(myName)
+print("Hello " + myName)
+length = len(myName)
+print("Length of your name is " + str(len(myName)))
+age = input("What is your age ")
+print("You will be " + str(int(age) + 1) + " next year")
