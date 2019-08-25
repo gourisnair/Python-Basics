37ea037fc4273423b318ccb52b28ee7ab843f106
gourisnair <gouri.gopan@gmail.com>
2019-08-25 23:42:01 +0530

    type casting

---
+++ TypeCasting.py
@@ -0,0 +1,20 @@
+x = 1
+y = 2.1
+z = 5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = float(y)
+y = int(y)
+z = complex(y)
+
+a = str(x)
+
+print(type(x))
+print(x)
+print(z)
+
+print(a)
+print(type(a))
